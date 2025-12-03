9c60f96891fd0fdb4bcec673ba524b0a336f5610
Samuelmooregaming-games <samweidmanqwe@gmail.com>
2025-12-02 22:58:33 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -29,7 +29,7 @@ document.querySelectorAll('.close-modal').forEach(close => {
 
         modal.querySelectorAll('video').forEach(v => {
     v.pause();
-    v.currentTime = 0;
+    v.currentTime = 1;
         });
     });
 });
